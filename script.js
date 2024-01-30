ac5acf5f3e0a8ab4cfb82bf5833766a21131094f
SvdNeo <sanjay.d@neoito.com>
2024-01-30 13:31:53 +0530

    provided styling for the HTML elements and created eventlistener

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const numberInput = document.getElementById('number-input');
+    const convertBtn = document.getElementById('convert-btn');
+    const resultOutput = document.getElementById('result');
+    const animationContainer = document.getElementById('animation-container');
+
+    convertBtn.addEventListener('click', checkUserInput);
+
+    
+    
+});
